f1a9ef0fdbd54d423cc3fa7be03607e064b6a39d
rufunt <jazzoo@mail.ru>
2022-11-22 16:37:57 +0300

    Add barbers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_003322) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_132307) do
+  create_table "barbers", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
